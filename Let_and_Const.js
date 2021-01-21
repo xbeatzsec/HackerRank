92521b6bef5fd910933855f6ec999ebdb286d955
xbeatzsec <75812403+xbeatzsec@users.noreply.github.com>
2021-01-21 23:18:46 +0000

    Create Let_and_Const.js

---
+++ Let_and_Const.js
@@ -0,0 +1,12 @@
+
+
+function main() {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    let r = Number(readLine())
+    // Print the area of the circle:
+    let area = (r ** 2) * Math.PI
+    console.log(area)
+    // Print the perimeter of the circle:
+    const perimeter = 2 * Math.PI * r 
+    console.log(perimeter)
+
